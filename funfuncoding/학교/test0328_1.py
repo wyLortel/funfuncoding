4cffe8538e4f4782608d864efe9a685ebac8f315
JEOUNG WOOUOUNG <young99@g.yju.ac.kr>
2025-04-27 16:28:22 +0900

    funfuncoding 폴더 추적 해제

--- funfuncoding/학교/test0328_1.py
+++
@@ -1,7 +0,0 @@
-#정사각형의 면적을 구하시오
-#가로 또는 세로의 길이를 입력 받는다
-square_side = int(input("정사각형의 가로를 입력해 주세요"))
-if square_side <= 0:
-    print("양수를 입력하세요")
-else:
-    print(f"정사각형의 면적은 {square_side*square_side} 입니다")
